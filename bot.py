d5f9c364074ead413ba68dc42e627d7fa89d007b
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-05-16 16:54:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,13 +31,13 @@ def start(client, message):
 @app.on_message(filters.regex("^https?:\/\/?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/).{11}"))
 def gyt(client,message):
       update_channel = CHANNEL
-	user_id = message.from_user.id
-	if update_channel :
-	  try:
+      user_id = message.from_user.id
+      if update_channel :
+	try:
               client.get_chat_member(update_channel, user_id)
-          except UserNotParticipant:
-	  	message.reply_text("**You Must Join My Updates Channel to use ME 😎 🤭** ",parse_mode="markdown", reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([ [ InlineKeyboardButton("Join My Updates Channel" ,url="https://t.me/My_Test_Botz") ]]))
-	  	return
+        except UserNotParticipant:
+	      message.reply_text("**You Must Join My Updates Channel to use ME 😎 🤭** ",parse_mode="markdown", reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([ [ InlineKeyboardButton("Join My Updates Channel" ,url="https://t.me/My_Test_Botz") ]]))
+	      return
 
 	try:
 		ms = message.reply_text("```checking valid link or not ```",reply_to_message_id = message.message_id )
